e47106524a8ccdab5edab70ad3811415fe754f13
Surote Wongpaiboon <surote.z9@gmail.com>
2023-03-07 15:08:33 +0700

    feat: return 200 some route

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 import (
     "github.com/labstack/echo/v4"
     "github.com/labstack/echo-contrib/prometheus"
+    "net/http"
 )
 func main() {
     e := echo.New()
     // Enable metrics middleware
+    e.GET("/users/:name", func(c echo.Context) error {
+        name := c.Param("name")
+        return c.String(http.StatusOK, name)
+    })
+
+    e.GET("/surote-cal", func(c echo.Context) error {
+        return c.String(http.StatusOK, "surote-cal is OK~")
+    })
     p := prometheus.NewPrometheus("echo", nil)
     p.Use(e)
 
